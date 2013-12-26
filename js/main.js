47fcd7b460aac1a0aaaceaf99db4592df18f6f8a
Nina Dang <ninadangt@gmail.com>
2013-12-25 18:00:01 -0800

    added toggle functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-$(document).ready(function() {
-  $('#media').carousel({
-    pause: true,
-    interval: false,
-  });
+$(function() {
+
+$("#day9").hide();
+$("#eSports").hide();
+
+  $("#day9button").click(function(){
+  		$("#eSports").hide();
+		$("#day9").toggle("slow");
+	});
+
+    $("#eSportsbutton").click(function(){
+  		$("#day9").hide();
+		$("#eSports").toggle("slow");
+	});
+
 });
